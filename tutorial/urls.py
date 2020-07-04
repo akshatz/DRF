76cd68fae32bf15d8b7b7fe90a432f0ca6309a69
Akshat Zala <akshatzala@gmail.com>
2020-07-04 20:51:38 +0530

    Added admin url

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from .quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -25,5 +26,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
